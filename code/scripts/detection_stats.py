cba998adb0e3dfc350b25b1fb373a8681bbdbf9a
Mitchell Metcalfe <mitchell.s.metcalfe@gmail.com>
2015-05-26 13:27:04 +1000

    Add hit rate approximation stub to detection_stats

--- code/scripts/detection_stats.py
+++ code/scripts/detection_stats.py
@@ -11,6 +11,7 @@ parser.add_argument('info_dat', type=str, nargs='?', default='info.dat', help='F
 args = parser.parse_args()
 
 # From: http://stackoverflow.com/a/312464
+# chunks :: [Int] -> [[Int]]
 def chunks(l, n):
     """ Yield successive n-sized chunks from l.
     """
@@ -18,6 +19,7 @@ def chunks(l, n):
         yield l[i:i+n]
 
 # Read a bounding box data file:
+# readDataFile :: String -> Map String [[Int]]
 def readDataFile(file_name):
 	data = {}
 	with open(file_name, 'r') as dat_file:
@@ -31,6 +33,7 @@ def readDataFile(file_name):
 			data[image_path] = objs
 	return data
 
+# corners :: [Int] -> [(Int, Int)]
 def corners(obj):
 	x = obj[0]
 	y = obj[1]
@@ -38,18 +41,23 @@ def corners(obj):
 	h = obj[3]
 	return [(x,y), (x+w,y), (x+w,y+h), (x,y+h)]
 
+# sqrDist :: (Int, Int) -> (Int, Int) -> Float
 def sqrDist(p1, p2):
 	return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
 
-def is_near(p1, p2):
+# isNear :: (Int, Int) -> (Int, Int) -> Bool
+def isNear(p1, p2):
 	return sqrDist(p1, p2) < MAX_BB_CORNER_DISTANCE^2
 
+# isDetection :: [Int] -> [Int] -> Bool
 def isDetection(obj, actual):
 	c_obj = corners(obj)
 	c_act = corners(actual)
 
-	return all(map(lambda (c1, c2): is_near(c1, c2), zip(c_obj, c_act)))
+	return all(map(lambda (c1, c2): isNear(c1, c2), zip(c_obj, c_act)))
 
+
+# Start script:
 detections = readDataFile(args.detections_dat)
 info = readDataFile(args.info_dat)
 
@@ -57,10 +65,14 @@ total_fp = 0
 total_tp = 0
 total_detected = 0
 
+total_objects = 0
+total_hit_count = 0
+
 for key in detections:
 	detected_objs = detections[key]
 	actual_objs = info['images/{}'.format(key)]
 
+	num_objects = len(actual_objs) + 0.0
 	num_detected = len(detected_objs) + 0.0
 
 	num_fp = 0
@@ -73,10 +85,20 @@ for key in detections:
 
 	total_fp += num_fp
 	total_tp += num_tp
+
+	# TODO: improve correctness of stats.
 	total_detected += num_detected
+	total_objects += num_objects
+
+	if num_tp > 0:
+		total_hit_count += 1 
 
 	print "img {:19}, tp: {:6.2f}, fp: {:6.2f}, tp%: {:4.2f}, fp%: {:4.2f}".format(key, num_tp, num_fp, num_tp / num_detected, num_fp / num_detected)
 	# print "img {}, tp: {:.2f}, fp: {:.2f}".format(key, num_tp, num_fp)
 	# print "img {}, tp: {:.2f}, fp: {:.2f}".format(key, num_tp, num_fp)
 
-print "TOTAL, tp%: {:.3f}, fp%: {:.3f}".format(total_tp / total_detected, total_fp / total_detected)
+# print "TOTAL, tp%: {:.3f}, fp%: {:.3f}".format(total_tp / total_detected, total_fp / total_detected)
+
+# TODO: This should be: (num objects which had an associated true positive) / (num objects)
+hit_rate = total_hit_count / total_objects
+print "hit_rate:", hit_rate
